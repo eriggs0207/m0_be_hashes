79748efe1eca91e73d4648c9210b808220ebc069
Erik Riggs <riggs.erik@gmail.com>
2022-06-09 21:22:53 -0600

    Cleaning up syntax on hashes

--- syntax_cleanup.rb
+++ syntax_cleanup.rb
@@ -1,23 +1,34 @@
 # Clean Up the code below to improve readability.
-foods = {"apples": 23, 
-  "grapes": 507, 
-  "eggs": 48}
+foods = {
+  "apples": 23,
+  "grapes": 507,
+  "eggs": 48
+}
 
-person = { "height": "6 ft", "weight": "160 lbs" }
+p foods
 
-post = { image_src: "./images/beach.png",
+person = {
+  "height": "6 ft",
+   "weight": "160 lbs"
+ }
 
+p person
 
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
+post = {
+  image_src: "./images/beach.png",
+ caption: "At the beach with my besties",
+ timestamp: "4:37 PM August 13, 2019",
         number_likes: 0,
-comments: [] }
+comments: []
+}
+p post 
 
-
-post2 =
-{
+post2 ={
       image_src: "./images/holiday-party.png",
       caption: "What a great holiday party omg",
       timestamp: "11:37 PM December 31, 2019",
       number_likes: 13,
       comments: []
 }
+
+p post2
